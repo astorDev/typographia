74bdd5871e3a2d069f7d42072e9ea67914aec2a8
Egor Tarasov <pavluchenk0@yandex.ru>
2024-06-21 18:12:52 +0400

    initial take on typographia

--- material/typographia/lib/main.dart
+++ material/typographia/lib/main.dart
@@ -1,16 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:typographia/typographia.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+List<Widget> all = [
+  TextDisplayLarge('Display Large\nopacity: 0.5', opacity: 0.5),
+  TextDisplayMedium('Display Medium\ncolor: orange[600]', color: Colors.orange[600]),
+  TextDisplaySmall('Display Small\nbackgroundColor: blue[100]', backgroundColor: Colors.blue[100]),
+  TextHeadlineLarge('Headline Large\ndecoration: TextDecoration.underline', decoration: TextDecoration.underline),
+  TextHeadlineMedium('Headline Medium. \ndirection: TextDirection.rtl', direction: TextDirection.rtl),
+  TextHeadlineSmall('Headline Small. overflow: TextOverflow.ellipsis. Plus some jibber-jabber to overflow', overflow: TextOverflow.ellipsis),
+  TextTitleLarge('Title Large. softWrap: false. Plus some jibber-jabber to overflow', softWrap: false),
+  TextTitleMedium('Title Medium. textAlign: TextAlign.center', textAlign: TextAlign.center),
+  TextTitleSmall('Title Small'),
+  TextBodyLarge('BodyLarge'),
+  TextBodyMedium('BodyMedium'),
+  TextBodySmall('BodySmall'),
+  TextLabelLarge('LabelLarge'),
+  TextLabelMedium('LabelMedium'),
+  TextLabelSmall('LabelSmall'),
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var columnChildren = all.reversed.map((e) => 
+      Padding(
+        padding: const EdgeInsets.only(bottom: 16), 
+        child: SizedBox(
+          width: 600,
+          child: e
+        )
+      )
+    ).toList();
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Typographia',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -20,8 +49,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Typographia'),
         ),
-        body: Center(
-          child: Text('Hello from Typographia!'),
+        body: Padding(
+          padding: const EdgeInsets.all(32),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: columnChildren
+              ),
+            ),
+          ),
         ),
       )
     );
---
+++ material/typographia/lib/typographia.dart
@@ -0,0 +1,335 @@
+import 'package:flutter/material.dart';
+
+abstract class TextWithKnownStyle extends StatelessWidget {
+  final String data;
+  final Color? color;
+  final Color? backgroundColor;
+  final double opacity;
+  final TextDecoration? decoration;
+  final TextDirection? direction;
+  final TextAlign? textAlign;
+  final TextOverflow? overflow;
+  final bool? softWrap;
+  
+  const TextWithKnownStyle(
+    this.data, {
+      super.key,
+      this.color,
+      this.backgroundColor,
+      this.decoration,
+      this.direction,
+      this.textAlign,
+      this.overflow,
+      this.softWrap,
+      this.opacity = 1,
+    });
+
+  TextStyle styleFrom(TextTheme theme);
+
+  @override
+  Widget build(BuildContext context) {
+    var sourceStyle = styleFrom(Theme.of(context).textTheme);
+
+    var appliedStyle = sourceStyle.copyWith(
+      // inherit
+      color: color ?? sourceStyle.color!.withOpacity(opacity),
+      backgroundColor: backgroundColor,
+      // fontSize
+      // fontWeight
+      // fontStyle
+      // letterSpacing
+      // wordSpacing
+      // textBaseline
+      // height
+      // leadingDistribution
+      // locale
+      // foreground (Paint)
+      // background (Paint)
+      decoration: decoration,
+      // decorationColor
+      // decorationStyle
+      // decorationThickness
+      // debugLabel
+      // fontFamily
+      // fontFamilyFallback
+      // package
+      // overflow
+    );
+
+    return Text(
+      data, 
+      style: appliedStyle,
+      // strutStyle
+      textAlign: textAlign,
+      textDirection: direction,
+      // locale
+      softWrap: softWrap,
+      overflow: overflow,
+      // textScaleFactor
+      // textScaler
+      // maxLines
+      // semanticsLabel
+      // textWidthBasis
+      // textHeightBehavior
+      // selectionColor
+    );
+  }
+}
+
+class TextDisplayLarge extends TextWithKnownStyle {
+  const TextDisplayLarge(
+    super.data, {
+      super.key,
+      super.color,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.displayLarge!;
+}
+
+class TextDisplayMedium extends TextWithKnownStyle {
+  const TextDisplayMedium(
+    super.data, {
+      super.key,
+      super.color,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.displayMedium!;
+}
+
+class TextDisplaySmall extends TextWithKnownStyle {
+  const TextDisplaySmall(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.displaySmall!;
+}
+
+class TextHeadlineLarge extends TextWithKnownStyle {
+  const TextHeadlineLarge(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.headlineLarge!;
+}
+
+class TextHeadlineMedium extends TextWithKnownStyle {
+  const TextHeadlineMedium(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.headlineMedium!;
+}
+
+class TextHeadlineSmall extends TextWithKnownStyle {
+  const TextHeadlineSmall(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.overflow,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.headlineSmall!;
+}
+
+class TextTitleLarge extends TextWithKnownStyle {
+  const TextTitleLarge(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.titleLarge!;
+}
+
+class TextTitleMedium extends TextWithKnownStyle {
+  const TextTitleMedium(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.titleMedium!;
+}
+
+class TextTitleSmall extends TextWithKnownStyle {
+  const TextTitleSmall(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.titleSmall!;
+}
+
+class TextBodyLarge extends TextWithKnownStyle {
+  const TextBodyLarge(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.bodyLarge!;
+}
+
+class TextBodyMedium extends TextWithKnownStyle {
+  const TextBodyMedium(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.bodyMedium!;
+}
+
+class TextBodySmall extends TextWithKnownStyle {
+  const TextBodySmall(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.bodySmall!;
+}
+
+class TextLabelLarge extends TextWithKnownStyle {
+  const TextLabelLarge(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.labelLarge!;
+}
+
+class TextLabelMedium extends TextWithKnownStyle {
+  const TextLabelMedium(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.labelMedium!;
+}
+
+class TextLabelSmall extends TextWithKnownStyle {
+  const TextLabelSmall(
+    super.data, {
+      super.key,
+      super.color,
+      super.backgroundColor,
+      super.decoration,
+      super.direction,
+      super.textAlign,
+      super.overflow,
+      super.softWrap,
+      super.opacity = 1,
+    }
+  );
+
+  @override
+  TextStyle styleFrom(TextTheme theme) => theme.labelSmall!;
+}
